c8214ad9cc3d482eb9d818a921ef7d90540524cf
DAVNGU99 <129026556+DAVNGU99@users.noreply.github.com>
2024-03-13 01:14:15 +0100

    lagt til oppgave 1C

--- src/Oppgave1/JavaSetToMengde.java
+++ src/Oppgave1/JavaSetToMengde.java
@@ -1,102 +1,165 @@
 package Oppgave1;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class JavaSetToMengde<T> implements MengdeADT<T> {
 
-	
-	private Set<T> set = new HashSet<T>();	
-	
-	
+	private Set<T> set = new HashSet<T>();
+
 	@Override
 	public boolean erTom() {
-	
+
 		return set.isEmpty();
-		
+
 	}
 
 	@Override
 	public boolean inneholder(T element) {
-		
+
 		return set.contains(element);
 	}
 
 	@Override
 	public boolean erDelmengdeAv(MengdeADT<T> annenMengde) {
-		
-		for (int i = 0; i < set.size(); i++) {
-			if(annenMengde.inneholder(set.iterator())) {
-				
-			}
-		}
-		
+	    for (T element : set) {
+	        if (!annenMengde.inneholder(element)) {
+	            return false;
+	        }
+	    }
+	    return true;
 	}
+
 
 	@Override
 	public boolean erLik(MengdeADT<T> annenMengde) {
-		// TODO Auto-generated method stub
-		return false;
+
+		T[] mengde = annenMengde.tilTabell();
+
+		if (set.size() != mengde.length) {
+			return false;
+		}
+
+		for (int i = 0; i < set.size(); i++) {
+			if (!set.contains(mengde[i])) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	@Override
 	public boolean erDisjunkt(MengdeADT<T> annenMengde) {
-		// TODO Auto-generated method stub
-		return false;
+
+		T[] mengde = annenMengde.tilTabell();
+
+		for (int i = 0; i < mengde.length; i++) {
+			if (set.contains(mengde[i])) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
 	public MengdeADT<T> snitt(MengdeADT<T> annenMengde) {
-		// TODO Auto-generated method stub
-		return null;
+
+		T[] mengde = annenMengde.tilTabell();
+		MengdeADT<T> temp = new TabellMengde<>();
+
+		for (int i = 0; i < mengde.length; i++) {
+			if (set.contains(mengde[i])) {
+				temp.leggTil(mengde[i]);
+			}
+		}
+		return temp;
+
 	}
 
 	@Override
 	public MengdeADT<T> union(MengdeADT<T> annenMengde) {
-		// TODO Auto-generated method stub
-		return null;
+
+		T[] mengde = annenMengde.tilTabell();
+		MengdeADT<T> temp = new TabellMengde<>(set.size() + mengde.length);
+
+		for (T x : mengde) {
+			temp.leggTil(x);
+		}
+
+		for (T tab : set) {
+			temp.leggTil(tab);
+		}
+
+		return temp;
+
 	}
 
 	@Override
 	public MengdeADT<T> minus(MengdeADT<T> annenMengde) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		T[] mengde = annenMengde.tilTabell();
+		MengdeADT<T> temp = new TabellMengde<>();
+		
+	
+		for(T element : set) {
+			if(!annenMengde.inneholder(element)) {
+				temp.leggTil(element);
+			}
+		}
+		return temp;
+		
 	}
 
 	@Override
 	public void leggTil(T element) {
-		// TODO Auto-generated method stub
-		
+
+		set.add(element);
+
 	}
 
 	@Override
 	public void leggTilAlleFra(MengdeADT<T> annenMengde) {
-		// TODO Auto-generated method stub
 		
+		T[] mengde = annenMengde.tilTabell();
+
+		
+		for(T temp : mengde) {
+			if(!set.contains(temp)) {
+				set.add(temp);
+			}
+		}
+		
+		
+
 	}
 
 	@Override
 	public T fjern(T element) {
-		// TODO Auto-generated method stub
+		
+	if(set.contains(element)) {
+		set.remove(element);
+		return element;
+	}
+		
 		return null;
+		
 	}
 
 	@Override
 	public T[] tilTabell() {
-		// TODO Auto-generated method stub
-		return null;
+
+		T[] tab = (T[]) new Object[set.size()];
+
+		return set.toArray(tab);
+
 	}
 
 	@Override
 	public int antallElementer() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return set.size();
 	}
 
-	
-	
-	
-	
-	
-	
 }
